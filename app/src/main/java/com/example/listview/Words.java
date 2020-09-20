2ed45c044639f4cbe46a5156101dd4e747f80662
1211186431 <56494120+1211186431@users.noreply.github.com>
2020-09-20 17:08:10 +0800

    9.20 contentprovide

--- app/src/main/java/com/example/listview/Words.java
+++ app/src/main/java/com/example/listview/Words.java
@@ -1,8 +1,30 @@
 package com.example.listview;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class Words {
+    public static final String AUTHORITY = "com.example.listview.provider";//URI授权者
+
+    public static abstract class Word implements BaseColumns {
+        public static final String TABLE_NAME = "words";//表名
+        public static final String COLUMN_NAME_WORD = "word";//列：单词
+        public static final String COLUMN_NAME_MEANING = "meaning";//列：单词含义
+        public static final String COLUMN_NAME_SAMPLE = "sample";//列：单词示例
+        // MIME类型
+        public static final String MIME_DIR_PREFIX = "vnd.android.cursor.dir";
+        public static final String MIME_ITEM_PREFIX = "vnd.android.cursor.item";
+        public static final String MINE_ITEM = "vnd.com.example.listview.words";
+        public static final String MINE_TYPE_SINGLE = MIME_ITEM_PREFIX + "/" + MINE_ITEM;
+        public static final String MINE_TYPE_MULTIPLE = MIME_DIR_PREFIX + "/" + MINE_ITEM;
+        public static final String PATH_SINGLE = "words/#";//单条数据的路径
+        public static final String PATH_MULTIPLE = "words";//多条数据的路径
+        // Content Uri
+        public static final String CONTENT_URI_STRING = "content://" + AUTHORITY + "/" + PATH_MULTIPLE;
+        public static final Uri CONTENT_URI = Uri.parse(CONTENT_URI_STRING);
+    }
+
+
     //单词列表项
     public static class WordItem {
         public String id;
@@ -67,13 +89,6 @@ public class Words {
         }
     }
 
-    public static abstract class Word implements BaseColumns {
-        public static final String TABLE_NAME = "words";//表名
-        // _ID字段：主键
-        public static final String COLUMN_NAME_WORD = "word";//字段：单词
-        public static final String COLUMN_NAME_MEANING = "meaning";//字段：单词含义
-        public static final String COLUMN_NAME_SAMPLE = "sample";//字段：单词示例
-    }
 
 
 }
